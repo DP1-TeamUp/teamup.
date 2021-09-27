7dafbf2e196d22ae363bfad5fa8f3e177f253718
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-09-27 23:19:46 +0600

    updated auth API

--- code/client/src/API/auth-helper.js
+++ code/client/src/API/auth-helper.js
@@ -1,16 +1,25 @@
-  
-const auth = {
-    authenticate(jwt) {
-      if (typeof window !== 'undefined') {
-        sessionStorage.setItem('jwt', JSON.stringify(jwt));
-      }
-    },
-  };
-  
-  const authenticate = async (jwt) => {
-    if (typeof window !== 'undefined') {
-      await sessionStorage.setItem('jwt', JSON.stringify(jwt));
-    }
-  };
-  
-  export { authenticate };
+import { signout } from './auth';
+
+const authenticate = async (jwt) => {
+  if (typeof window !== 'undefined') {
+    await sessionStorage.setItem('jwt', JSON.stringify(jwt));
+  }
+};
+
+const isAuthenticated = () => {
+  if (typeof window == 'undefined') return false;
+  if (sessionStorage.getItem('jwt'))
+    return JSON.parse(sessionStorage.getItem('jwt'));
+  else return false;
+};
+
+const clearJWT = (cb) => {
+  if (typeof window !== 'undefined') sessionStorage.removeItem('jwt');
+  cb();
+  //optional
+  signout().then((data) => {
+    document.cookie = 't=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+  });
+};
+
+export { authenticate, isAuthenticated, clearJWT };
