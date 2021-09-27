842ba076406694d048bb40facaf847cff6d16e92
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-09-27 23:21:48 +0600

    worked on user API

--- code/client/src/API/user.js
+++ code/client/src/API/user.js
@@ -1,35 +1,129 @@
 const create = async (user) => {
-    try {
-      let response = await fetch('http://localhost:5000/api/users/', {
-        method: 'POST',
+  try {
+    let response = await fetch('http://localhost:5000/api/users/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const list = async (signal) => {
+  try {
+    let response = await fetch('http://localhost:5000/api/users/', {
+      method: 'GET',
+      signal: signal,
+    });
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const read = async () => {};
+
+const update = async (params, credentials, user) => {
+  try {
+    let response = await fetch(
+      'http://localhost:5000/api/users/' + params.userId,
+      {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer ' + credentials.t,
+        },
+        body: user,
+      }
+    );
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const remove = async () => {};
+
+const forgetPassword = async (body) => {
+  try {
+    let response = await fetch('http://localhost:5000/forget-password', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const resetPassword = async (body) => {
+  try {
+    let response = await fetch(
+      `http://localhost:5000/reset-password/${body.resetToken}`,
+      {
+        method: 'PUT',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(user),
-      });
-      return await response.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const list = async (signal) => {
-    try {
-      let response = await fetch('http://localhost:5000/api/users/', {
-        method: 'GET',
-        signal: signal,
-      });
-      return await response.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const read = async () => {};
-  
-  const update = async () => {};
-  
-  const remove = async () => {};
-  
-  export { create, list, read, update, remove };
+        body: JSON.stringify(body),
+      }
+    );
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addSkill = async (body) => {
+  try {
+    let response = await fetch(`http://localhost:5000/api/skills/users`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteSkill = async (body) => {
+  try {
+    let response = await fetch(`http://localhost:5000/api/skills/users`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export {
+  create,
+  list,
+  read,
+  update,
+  remove,
+  forgetPassword,
+  resetPassword,
+  addSkill,
+  deleteSkill,
+};
